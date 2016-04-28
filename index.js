9e40f9c45293bf2d1d4418b637d22a611374213f
Raja Sekar <rajasekar89be@gmail.com>
2016-04-28 20:42:53 +0530

    readme updated

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _react = require('react');
+var _react = require("react");
 
 var _react2 = _interopRequireDefault(_react);
 
@@ -28,17 +28,62 @@ var ReactDisqus = function (_React$Component) {
   }
 
   _createClass(ReactDisqus, [{
-    key: 'render',
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      this.embedDisqus();
+    }
+  }, {
+    key: "render",
     value: function render() {
       return _react2.default.createElement(
-        'div',
+        "div",
         null,
-        'React Disqus'
+        _react2.default.createElement(
+          "div",
+          { id: "disqus_thread" },
+          _react2.default.createElement(
+            "noscript",
+            null,
+            _react2.default.createElement(
+              "span",
+              null,
+              "Please enable JavaScript to view the",
+              _react2.default.createElement(
+                "a",
+                { href: "http://disqus.com/?ref_noscript" },
+                "comments powered by Disqus."
+              )
+            )
+          )
+        )
       );
+    }
+  }, {
+    key: "embedDisqus",
+    value: function embedDisqus() {
+      var disqus_config = function disqus_config() {
+        this.page.url = this.props.pageurl;
+        this.page.identifier = this.props.identifier;
+      };
+      var dsq = document.createElement('script');dsq.type = 'text/javascript';dsq.async = true;
+      dsq.src = 'http://' + this.props.shortname + '.disqus.com/embed.js';
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
     }
   }]);
 
   return ReactDisqus;
 }(_react2.default.Component);
+
+ReactDisqus.propTypes = {
+  shortname: _react2.default.PropTypes.string.isRequired,
+  pageurl: _react2.default.PropTypes.string,
+  identifier: _react2.default.PropTypes.string
+
+};
+ReactDisqus.defaultProps = {
+  shortname: null,
+  pageurl: window.location.href,
+  identifier: null
+};
 
 exports.default = ReactDisqus;
